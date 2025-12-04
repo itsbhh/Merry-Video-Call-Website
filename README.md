44e10eb8208fc30421d12b411c16c7bcfd563628
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2025-12-04 17:57:31 +0530

    BACKEND

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This project is developed while learning from Apna College.
 ![image](https://github.com/user-attachments/assets/ded9f725-ed66-4eb7-b7f7-5732ec67db2b)
 
  
+A project made while learning from Apna College.
--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -22,18 +23,17 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("MONGOURI")
+  try {
+    const connectionDb = await mongoose.connect(MONGO_URI);
+    console.log(`MONGO Connected: ${connectionDb.connection.host}`);
 
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENING ON PORT 8000")
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
-
-
-
-}
-
-
+  } catch (error) {
+    console.log("Database connection failed", error);
+    process.exit(1);
+  }
+};
 
 start();
